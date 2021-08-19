98574cd25130e8aea4d4c3dbd6157c1372703be5
Sergey Ivashko <grayseneov@gmail.com>
2021-08-19 12:41:34 +0300

    insert card description to dynamic position

--- scripts/dist/index.dev.js
+++ scripts/dist/index.dev.js
@@ -3,24 +3,56 @@
 var deliveryCardKeys = ["pickUp", "courierCity", "courierCountry"];
 deliveryCardKeys.forEach(function (key) {
   var cardEl = document.getElementById(key);
-  cardEl.addEventListener("click", function () {
-    deliveryCardKeys.forEach(function (key) {
-      document.getElementById(key).classList.remove("a-card--active");
-      document.getElementById("".concat(key, "Form")).classList.remove("a-d-block");
+
+  if (cardEl) {
+    cardEl.addEventListener("click", function (e) {
+      deliveryCardKeys.forEach(function (key) {
+        var _cardEl = document.getElementById(key);
+
+        if (_cardEl) {
+          document.getElementById(key).classList.remove("a-card--active");
+          document.getElementById("".concat(key, "Form")).classList.remove("a-d-block");
+        }
+      });
+      var innerEl = document.getElementById("".concat(key, "Form"));
+      var mql = window.matchMedia("screen and (max-width: 576px)");
+      var targetEl = mql.matches ? e.currentTarget : document.getElementById("deliveryCards");
+      targetEl.after(innerEl);
+      cardEl.classList.add("a-card--active");
+      innerEl.classList.add("a-d-block");
     });
-    cardEl.classList.add("a-card--active");
-    document.getElementById("".concat(key, "Form")).classList.add("a-d-block");
-  });
+  }
 });
 var paymentCardKeys = ["cash", "credit", "installmentPlan", "creditCard", "onlineCard", "cashless"];
 paymentCardKeys.forEach(function (key) {
   var cardEl = document.getElementById(key);
-  cardEl.addEventListener("click", function () {
-    paymentCardKeys.forEach(function (key) {
-      document.getElementById(key).classList.remove("a-card--active");
-      document.getElementById("".concat(key, "Form")).classList.remove("a-d-block");
+
+  if (cardEl) {
+    cardEl.addEventListener("click", function (e) {
+      paymentCardKeys.forEach(function (key) {
+        var _cardEl = document.getElementById(key);
+
+        if (_cardEl) {
+          document.getElementById(key).classList.remove("a-card--active");
+          document.getElementById("".concat(key, "Form")).classList.remove("a-d-block");
+        }
+      });
+      var innerEl = document.getElementById("".concat(key, "Form"));
+      var mql = window.matchMedia("screen and (max-width: 576px)");
+      var targetEl = mql.matches ? e.currentTarget : document.getElementById("paymentCards");
+      targetEl.after(innerEl);
+      cardEl.classList.add("a-card--active");
+      innerEl.classList.add("a-d-block");
     });
-    cardEl.classList.add("a-card--active");
-    document.getElementById("".concat(key, "Form")).classList.add("a-d-block");
+  }
+});
+window.matchMedia("screen and (max-width: 576px)").addEventListener("change", function () {
+  deliveryCardKeys.forEach(function (key) {
+    document.getElementById(key).classList.remove("a-card--active");
+    document.getElementById("".concat(key, "Form")).classList.remove("a-d-block");
+  });
+  paymentCardKeys.forEach(function (key) {
+    document.getElementById(key).classList.remove("a-card--active");
+    document.getElementById("".concat(key, "Form")).classList.remove("a-d-block");
   });
 });
--- scripts/index.js
+++ scripts/index.js
@@ -2,27 +2,68 @@ const deliveryCardKeys = ["pickUp", "courierCity", "courierCountry"];
 
 deliveryCardKeys.forEach((key) => {
   const cardEl = document.getElementById(key);
-  cardEl.addEventListener("click", () => {
+  if (cardEl) {
+    cardEl.addEventListener("click", (e) => {
+      deliveryCardKeys.forEach((key) => {
+        const _cardEl = document.getElementById(key);
+        if (_cardEl) {
+          document.getElementById(key).classList.remove("a-card--active");
+          document.getElementById(`${key}Form`).classList.remove("a-d-block");
+        }
+      });
+      const innerEl = document.getElementById(`${key}Form`);
+      const mql = window.matchMedia("screen and (max-width: 576px)");
+      const targetEl = mql.matches
+        ? e.currentTarget
+        : document.getElementById("deliveryCards");
+      targetEl.after(innerEl);
+      cardEl.classList.add("a-card--active");
+      innerEl.classList.add("a-d-block");
+    });
+  }
+});
+
+const paymentCardKeys = [
+  "cash",
+  "credit",
+  "installmentPlan",
+  "creditCard",
+  "onlineCard",
+  "cashless",
+];
+
+paymentCardKeys.forEach((key) => {
+  const cardEl = document.getElementById(key);
+  if (cardEl) {
+    cardEl.addEventListener("click", (e) => {
+      paymentCardKeys.forEach((key) => {
+        const _cardEl = document.getElementById(key);
+        if (_cardEl) {
+          document.getElementById(key).classList.remove("a-card--active");
+          document.getElementById(`${key}Form`).classList.remove("a-d-block");
+        }
+      });
+      const innerEl = document.getElementById(`${key}Form`);
+      const mql = window.matchMedia("screen and (max-width: 576px)");
+      const targetEl = mql.matches
+        ? e.currentTarget
+        : document.getElementById("paymentCards");
+      targetEl.after(innerEl);
+      cardEl.classList.add("a-card--active");
+      innerEl.classList.add("a-d-block");
+    });
+  }
+});
+
+window
+  .matchMedia("screen and (max-width: 576px)")
+  .addEventListener("change", () => {
     deliveryCardKeys.forEach((key) => {
       document.getElementById(key).classList.remove("a-card--active");
       document.getElementById(`${key}Form`).classList.remove("a-d-block");
     });
-    cardEl.classList.add("a-card--active");
-    document.getElementById(`${key}Form`).classList.add("a-d-block");
-  });
-});
-
-
-const paymentCardKeys = ["cash", "credit", "installmentPlan", "creditCard", "onlineCard", "cashless"];
-
-paymentCardKeys.forEach((key) => {
-  const cardEl = document.getElementById(key);
-  cardEl.addEventListener("click", () => {
     paymentCardKeys.forEach((key) => {
       document.getElementById(key).classList.remove("a-card--active");
       document.getElementById(`${key}Form`).classList.remove("a-d-block");
     });
-    cardEl.classList.add("a-card--active");
-    document.getElementById(`${key}Form`).classList.add("a-d-block");
   });
-});
